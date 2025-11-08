b0bc2f6a77ec0400ef722376e6a83fefd2b0bb9c
reyhane9015 <rafati.reyhaneh@yahoo.com>
2025-11-08 20:54:01 +0330

    add memo to listitem,list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+import { type ReactNode, memo } from "react";
 
 import IconButton from "@/components/IconButton/IconButton.tsx";
 
@@ -14,7 +14,7 @@ type Props = {
   list: ListType;
 };
 
-export default function List({ list }: Props): ReactNode {
+const List = memo(function List({ list }: Props): ReactNode {
   return (
     <div className={styles.list}>
       {" "}
@@ -33,4 +33,6 @@ export default function List({ list }: Props): ReactNode {
       </ul>
     </div>
   );
-}
+});
+
+export default List;
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+import { type ReactNode, memo } from "react";
 
 import type { ListItemType } from "@/types/list-item";
 
@@ -8,6 +8,8 @@ type Props = {
   item: ListItemType;
 };
 
-export default function ListItem({ item }: Props): ReactNode {
+const ListItem = memo(function ListItem({ item }: Props): ReactNode {
   return <div className={styles["list-item"]}>{item.title}</div>;
-}
+});
+
+export default ListItem;
